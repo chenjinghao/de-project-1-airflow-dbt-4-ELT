9f4e399e2773dc249e8ab3062a08d18cff103a6f
ADMA JINGHAO CHEN <ADAM_CJH@OUTLOOK.COM>
2026-02-08 22:04:27 -0800

    ✨ Modify database creation command to check for existence before creation in init-postgres.sh

--- scripts/init-postgres.sh
+++ scripts/init-postgres.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE stocks_db;
+    SELECT 'CREATE DATABASE stocks_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stocks_db')\gexec
     GRANT ALL PRIVILEGES ON DATABASE stocks_db TO $POSTGRES_USER;
 EOSQL
